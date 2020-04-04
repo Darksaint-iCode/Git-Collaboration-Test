7a42a65fe02d6ffa75b8468f109041f6456c67a0
Darksaint-iCode <ibroyusufola@gmail.com>
2020-04-04 13:15:10 -0700

    Ibro: I have done my part ;)

--- src/clicaltest/CLICalTest.java
+++ src/clicaltest/CLICalTest.java
@@ -16,12 +16,93 @@ public class CLICalTest {
     /**
      * @param args the command line arguments
      */
-    
+    static double a,b,c;
+    static Scanner perOp = new Scanner(System.in);
     
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        Start();
     }
     
-    
+    private static void Start(){
+        Scanner inputObja = new Scanner(System.in);
+        
+        System.out.println("Welcome User :)");
+        System.out.print("Input First number: ");
+        
+        a = inputObja.nextInt();
+        
+        System.out.print("\nInput Second number: ");
+        
+        b = inputObja.nextInt();
+        
+        System.out.println("\nWhat operation are you willing to Perform User :)");
+        System.out.println("Input (1) for Addition");
+        System.out.println("Input (2) for Subtraction");
+        System.out.println("Input (3) for Multiplication");
+        System.out.println("Input (4) for Division");
+        System.out.println("Input (5) for Modulus \n");
+        
+        System.out.print("Input a Number to Perform an Operation User: ");
+        
+        int sOperation = inputObja.nextInt();
+        
+        switch(sOperation){
+            case 1: perAddition();
+                break;
+            case 2: perSubtraction();
+                break;
+            case 5: perModulous();
+                break;
+            default: System.out.println("Invalid Input :(");
+        } 
+    }
+
+    private static void perAddition() {
+        c = a + b;
+        System.out.println("\nYour answer is: " + c + "\n");
+        System.out.println("Do you wanna perform another Operation?");
+        System.out.print("Input 1 for Yes and 2 for No: ");
+        
+        int perTrans = perOp.nextInt();
+        
+        if(perTrans == 1){
+            System.out.println("");
+            Start();
+        }else{
+            System.exit(0);
+        }
+    }
+
+    private static void perSubtraction() {
+        c = a - b;
+        System.out.println("\nYour answer is: " + c + "\n");
+        System.out.println("Do you wanna perform another Operation?");
+        System.out.print("Input 1 for Yes and 2 for No: ");
+        
+        int perTrans = perOp.nextInt();
+        
+        if(perTrans == 1){
+            System.out.println("");
+            Start();
+        }else{
+            System.exit(0);
+        }
+    }
+
+    private static void perModulous() {
+        c = a % b;
+        System.out.println("\nYour answer is: " + c + "\n");
+        System.out.println("Do you wanna perform another Operation?");
+        System.out.print("Input 1 for Yes and 2 for No: ");
+        
+        int perTrans = perOp.nextInt();
+        
+        if(perTrans == 1){
+            System.out.println("");
+            Start();
+        }else{
+            System.exit(0);
+        }
+    }
 }
